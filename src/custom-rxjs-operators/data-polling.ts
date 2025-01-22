eb5b4d00b060cbc7747618835558384ac0eba520
krivanek06 <krivanek1234@gmail.com>
2025-01-22 19:50:52 +0100

    changed polling pipe

--- src/custom-rxjs-operators/data-polling.ts
+++ src/custom-rxjs-operators/data-polling.ts
@@ -1,23 +1,29 @@
-import { MonoTypeOperatorFunction, switchMap, timer, tap } from "rxjs";
-import { HttpClient } from "@angular/common/http";
-import { inject } from "@angular/core";
+import {
+  MonoTypeOperatorFunction,
+  switchMap,
+  timer,
+  tap,
+  Observable,
+} from "rxjs";
 
 /**
  * continuously polls data from a provided URL
+ *
+ * @param data
+ *  - loader - a http call to load data
+ *  - reloadSeconds - how often to poll the server
  */
 export function dataPolling<T>(data: {
-  url: string;
+  loader: () => Observable<T>;
   reloadSeconds: number;
 }): MonoTypeOperatorFunction<T> {
-  const http = inject(HttpClient);
-
   return (source) =>
     source.pipe(
       switchMap(() =>
         timer(0, data.reloadSeconds * 1000).pipe(
           // log reloading
           tap(() => console.log("reloading", new Date())),
-          switchMap(() => http.get<T>(data.url))
+          switchMap(data.loader)
         )
       )
     );
